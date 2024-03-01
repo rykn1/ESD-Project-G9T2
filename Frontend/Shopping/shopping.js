d1902568838db1268b05be1f1c2a9e848bdab5b4
Koh Renyi <kohrenyi0802@gmail.com>
2024-03-01 21:21:42 +0800

    Basic Set Up Shopping Cart -> Payment

--- Frontend/Shopping/shopping.js
+++ Frontend/Shopping/shopping.js
@@ -2,14 +2,14 @@ new Vue({
     el: '#app',
     data: {
       items: [
-        { id: 1, name: 'Item 1', price: 10, quantity: 1 },
-        { id: 2, name: 'Item 2', price: 20, quantity: 1 },
-        { id: 3, name: 'Item 3', price: 15, quantity: 1 },
-        { id: 4, name: 'Item 4', price: 25, quantity: 1 },
-        { id: 5, name: 'Item 5', price: 30, quantity: 1 },
-        { id: 6, name: 'Item 6', price: 12, quantity: 1 },
-        { id: 7, name: 'Item 7', price: 18, quantity: 1 },
-        { id: 8, name: 'Item 8', price: 22, quantity: 1 }
+        { id: 1, name: 'Travel Insurance Bronze', price: 82, quantity: 1 },
+        { id: 2, name: 'Neck Pillow', price: 14.99, quantity: 1 },
+        { id: 3, name: 'SQ One Way to Tokyo', price: 399, quantity: 1 },
+        { id: 4, name: 'ANA One Way to Tokyo', price: 402, quantity: 1 },
+        { id: 5, name: 'Ground Tour Seoul', price: 43.42, quantity: 1 },
+        { id: 6, name: 'Great Wall of China Pass', price: 15, quantity: 1 },
+        { id: 7, name: 'E-Sim Unlimited Wifi 10 Days', price: 23, quantity: 1 },
+        { id: 8, name: 'Travel Wifi Router 7 Days', price: 30, quantity: 1 }
       ],
       cart: []
     },
