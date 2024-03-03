b67154bbbd6bae2654e8bcf88c00f09b716e00d2
Koh Renyi <kohrenyi0802@gmail.com>
2024-03-03 21:30:15 +0800

    New Weather Microservice

---
+++ Frontend/Planner/weather.py
@@ -0,0 +1,33 @@
+
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import requests
+
+app = Flask(__name__)
+CORS(app)
+
+# Make your openweather API key
+OPENWEATHERMAP_API_KEY = ""
+
+@app.route('/weather', methods=['POST'])
+def get_weather():
+    data = request.get_json()
+    country = data.get('country')
+    if not country:
+        return jsonify({'error': 'Missing country'}), 400
+
+    api_url = f"https://api.openweathermap.org/data/2.5/weather?q={country}&appid={OPENWEATHERMAP_API_KEY}"
+
+    try:
+        response = requests.get(api_url) 
+        weather_data = response.json()
+        return jsonify({
+            'temperature': weather_data.main.temp,
+            'description': weather_data.weather[0].description
+        })
+    
+    except requests.exceptions.RequestException as e:
+        return jsonify({'error': 'Error fetching weather data'}), 500 
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5003)
