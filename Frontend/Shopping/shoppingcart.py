21998b9aa41421ae24352ebb8898d039f5233455
milkbaum <142380414+milkbaum@users.noreply.github.com>
2024-03-13 18:05:24 +0800

    updated shopping cart service

---
+++ Frontend/Shopping/shoppingcart.py
@@ -0,0 +1,122 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+
+from datetime import datetime
+import json
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:@localhost:3306/cart'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_ENGINE_OPTIONS"] = {"pool_recycle": 299}
+
+db = SQLAlchemy(app)
+
+CORS(app)
+
+class Cart(db.Model):
+    __tablename__ = 'cart'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), nullable=False)
+    price = db.Column(db.Float(precision=2), nullable=False)
+    quantity = db.Column(db.Integer)
+
+    def __init__(self, id, name, price, quantity):
+        self.id = id
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+
+    def json(self):
+        return {"id": self.id, "name": self.name, "price": self.price, "quantity": self.quantity}
+
+@app.route("/cart")
+def get_all ():
+    itemList = db.session.scalars(db.select(Cart)).all()
+
+    if len(itemList):
+        return jsonify(
+            {
+                "code": 200,
+                "data": {
+                    "item": [item.json() for item in itemList]
+                }
+            }
+        )
+    return jsonify(
+        {
+            "code": 404,
+            "message": "There are no items."
+        }
+    ), 404
+    
+@app.route("/cart", methods=['POST'])
+def add_items():
+    data = request.json
+    id = data.get('id')
+    name = data.get('name')
+    price = data.get('price')
+    quantity = data.get('quantity')  
+      
+    existing_item = Cart.query.filter_by(id=id).first()
+    new_item = None
+    if existing_item:
+        existing_item.quantity += quantity
+    else:
+        new_item = Cart(id=id, name=name, price=price, quantity=quantity)
+        db.session.add(new_item)
+        
+    try:
+        db.session.commit()
+    except Exception as e:
+        return jsonify(
+            {
+                "code": 500,
+                "message": "An error occurred while creating the order. " + str(e)
+            }
+        ), 500
+    
+    if new_item:  # Check if new_item is defined
+        print(json.dumps(new_item.json(), default=str)) # convert a JSON object to a string and print
+        print()
+
+    return jsonify(
+        {
+            "code": 201,
+            "data": new_item.json() if new_item else None  # Return new_item.json() if new_item is defined, otherwise return None
+        }
+    ), 201
+
+@app.route("/cart/<int:id>", methods=['DELETE'])
+def delete_item(id):
+    item = db.session.scalars(db.select(Cart).filter_by(id=id).limit(1)).first()
+    if item:
+        db.session.delete(item)
+        db.session.commit()
+        return jsonify(
+            {
+                "code": 200,
+                "data": {
+                    "id": id
+                }
+            }
+        )
+    return jsonify(
+        {
+            "code": 404,
+            "data": {
+                "id": id
+            },
+            "message": "Item not found."
+        }
+    ), 404
+
+@app.route("/totalprice", methods=['GET'])
+def get_total_price():
+    cart_items = Cart.query.all()
+    total_price = sum(item.price * item.quantity for item in cart_items)
+    return jsonify({"total_price": total_price}), 200
+
+if __name__ == '__main__':
+    app.run(port=5006, debug=True)
