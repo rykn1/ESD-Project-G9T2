108a74842a415e00f7fbd686c4d31c606c70bd99
milkbaum <142380414+milkbaum@users.noreply.github.com>
2024-03-13 22:22:31 +0800

    updated app.config to suit window users

--- Frontend/Shopping/shopItems.py
+++ Frontend/Shopping/shopItems.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:root@localhost:8889/item'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:@localhost:3306/item'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config["SQLALCHEMY_ENGINE_OPTIONS"] = {"pool_recycle": 299}
 
@@ -117,4 +117,4 @@ def create_book(id):
 
 
 if __name__ == '__main__':
-    app.run(port=5003, debug=True)
+    app.run(port=5005, debug=True)
