fc71b7701923b69c394a2e6b1de6a25c4784e60d
milkbaum <142380414+milkbaum@users.noreply.github.com>
2024-03-14 19:04:48 +0800

    updated payment service

---
+++ Frontend/Shopping/payment.py
@@ -0,0 +1,74 @@
+from flask import Flask, render_template, url_for, request, abort, redirect, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+import json
+
+import stripe
+
+app = Flask(__name__, static_url_path="",static_folder="public")
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:@localhost:3306/cart'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_ENGINE_OPTIONS"] = {"pool_recycle": 299}
+app.config['STRIPE_PUBLIC_KEY'] = 'pk_test_51OrELHATlCeKbEIxdOhbVW5Vii3DbYkWUtdqLtf88Mg4ATq96PtsfQRqbwbJbNikvmwedig7BQtED7vDb9zvQlKQ00FD5yU6c0'
+app.config['STRIPE_SECRET_KEY'] = 'rk_test_51OrELHATlCeKbEIxLxwiyGHRkrXW3Di18YfJdrzOxGvI9mjz8QfGMR07VLy3FXsMiuRDrfNTbps32m5HBpV0ComF006WAL1TfX'
+
+
+db = SQLAlchemy(app)
+
+CORS(app)
+
+class Cart(db.Model):
+    __tablename__ = 'cart'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), nullable=False)
+    price = db.Column(db.Float(precision=2), nullable=False)
+    quantity = db.Column(db.Integer)
+
+    def __init__(self, id, name, price, quantity):
+        self.id = id
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+
+    def json(self):
+        return {"id": self.id, "name": self.name, "price": self.price, "quantity": self.quantity}
+
+
+
+stripe.api_key = app.config['STRIPE_SECRET_KEY']
+
+
+
+@app.route('/create-checkout-session', methods=['POST'])
+def create_checkout_session():
+
+    try:
+        
+        cart_items = Cart.query.all()
+        line_items=[]
+        line_item={}
+        
+        for item in cart_items:
+            line_item ={
+                'price': item.id,
+                'quantity': item.quantity,
+            }
+            line_items.append(line_item)
+        print(line_items)
+        checkout_session=stripe.checkout.Session.create(
+            line_items=line_items,
+            mode='payment',
+            success_url= 'http://localhost:5007/thanks.html',
+            cancel_url="http://localhost:5007/cancel.html",
+
+        )
+        
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+    
+    return redirect(checkout_session.url, code=303)
+
+
+if __name__ == '__main__':
+    app.run(port=5007, debug=True)
--- Frontend/Shopping/shopping.sql
+++ Frontend/Shopping/shopping.sql
@@ -3,18 +3,18 @@ CREATE DATABASE IF NOT EXISTS item;
 USE item;
 
 CREATE TABLE IF NOT EXISTS item (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id VARCHAR(255) PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     price DECIMAL(10, 2) NOT NULL,
     quantity INT NOT NULL
 );
 
 INSERT INTO `item` (`id`, `name`, `price`, `quantity`) VALUES
-('1', 'Travel Insurance Bronze', '82.00', '1'),
-('2', 'Neck Pillow', '14.99', '1'),
-('3', 'SQ One Way to Tokyo', '399.00', '1'),
-('4', 'ANA One Way to Tokyo', '402.00', '1'),
-('5', 'Ground Tour Seoul', '43.42', '1'),
-('6', 'Great Wall of China Pass', '15.00', '1'),
-('7', 'E-Sim Unlimited Wifi 10 Days', '23.00', '1'),
-('8', 'Travel Wifi Router 7 Days', '30.00', '1');
+('price_1OsRBkATlCeKbEIxRqhWm7kZ', 'Travel Insurance Bronze', '82.00', '1'),
+('price_1OuAfoATlCeKbEIxhCUtH2hV', 'Neck Pillow', '14.99', '1'),
+('price_1OuAg6ATlCeKbEIxywMYo4Ql', 'SQ One Way to Tokyo', '399.00', '1'),
+('price_1OuAgKATlCeKbEIxKMzjAWzF', 'ANA One Way to Tokyo', '402.00', '1'),
+('price_1OuAgeATlCeKbEIxG3yFhvUZ', 'Ground Tour Seoul', '43.42', '1'),
+('price_1OuAgrATlCeKbEIxwEaLl9le', 'Great Wall of China Pass', '15.00', '1'),
+('price_1OuAh8ATlCeKbEIxeXu2tGvh', 'E-Sim Unlimited Wifi 10 Days', '23.00', '1'),
+('price_1OuAhOATlCeKbEIxatb7OYUQ', 'Travel Wifi Router 7 Days', '30.00', '1');
