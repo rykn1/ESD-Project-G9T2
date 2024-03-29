b913cb2a54d200f62f6e8f0747f080f01fe14df4
Koh Renyi <kohrenyi0802@gmail.com>
2024-03-29 12:28:29 +0800

    Implement Working Compose

--- PlannerComplex/planner.py
+++ PlannerComplex/planner.py
@@ -7,25 +7,26 @@ CORS(app)
 print('asd')
 @app.route('/api/plan', methods=['POST'])
 def plan_itinerary():
+    print('testgh')
     data = request.json
     result = processPlanRequest(data)
     return result
 
 def processPlanRequest(data):
 
-    geminiResult = requests.post('http://127.0.0.1:5002/plan', json=data)
+    geminiResult = requests.post('http://generative_ai:5002/plan', json=data)
     if geminiResult.status_code not in range(200,300):
         print("gemini error")
         return "gemini error"
     geminiResult = geminiResult.json()
-    weatherResult = requests.post('http://127.0.0.1:5004/weather', json=data)
+    weatherResult = requests.post('http://weather:5004/weather', json=data)
     if weatherResult.status_code not in range(200,300):
         print("weather error")
         return "weather error"
     weatherResult = weatherResult.json()
     print('wok')
     destCurr = geminiResult["CountryCurrency"]["CurrencySymbol"]
-    currencyResult = requests.get(f'http://127.0.0.1:5003/exchange?from=SGD&to={destCurr}&amount=1')
+    currencyResult = requests.get(f'http://currency_service:5003/exchange?from=SGD&to={destCurr}&amount=1')
     if currencyResult.status_code not in range(200,300):
         print("currency error")
         return "currency error"
