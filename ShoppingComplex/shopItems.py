1ff668ff38e7bae976034a9893437e5489760195
Koh Renyi <kohrenyi0802@gmail.com>
2024-03-29 17:24:28 +0800

    SHAG BRO

--- ShoppingComplex/shopItems.py
+++ ShoppingComplex/shopItems.py
@@ -5,6 +5,7 @@ from os import environ
 print('testtest')
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('dbURL') 
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root@localhost:3306/item'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config["SQLALCHEMY_ENGINE_OPTIONS"] = {"pool_recycle": 299}
 
--- ShoppingComplex/shoppingcart.py
+++ ShoppingComplex/shoppingcart.py
@@ -7,6 +7,7 @@ import json
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('dbURL')
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root@localhost:3306/cart'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config["SQLALCHEMY_ENGINE_OPTIONS"] = {"pool_recycle": 299}
 
